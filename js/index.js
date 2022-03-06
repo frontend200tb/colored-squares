f8512850c21eb39a038be20088f4cd7735c380d3
Иван Каширин <85587225+frontend200tb@users.noreply.github.com>
2022-03-06 16:05:40 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,76 @@
+console.log('start frontend200tb three elements');
+
+
+/*****************
+Константы
+*****************/
+const h1 = document.querySelector('.h1');
+const playground = document.querySelector('.playground');
+
+
+/*****************
+Переменные
+*****************/
+const min = 10;
+const max = 100;
+const squareSize = 10;
+let countSquares;
+let colors = ['#ff0000', '#800000', '#ffff00', '#808000', '#00ff00', '#008000', '#008080', '#0000ff', '#000080', '#ff00ff', '#800080', '#222222'];
+
+
+/*****************
+Функции
+*****************/
+function show($el) {
+  $el.classList.remove('hide')
+}
+
+function hide($el) {
+  $el.classList.add('hide')
+}
+
+
+function setGameScore() {
+  result.textContent = currentScore.toString()
+}
+
+
+function renderBox(count) {
+  for (let i = 1; i <= count; i++) {
+  let box = document.createElement('div');
+    let gameSize = playground.getBoundingClientRect()
+    let maxTop = gameSize.height - 10;
+    let maxLeft = gameSize.width - 10;
+    let randomColorIndex = getRandom(0, colors.length)
+  console.log(randomColorIndex)
+    box.style.height = box.style.width = squareSize + 'px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = colors[randomColorIndex]
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box', 'true')
+
+    playground.insertAdjacentElement('afterbegin', box)
+  }
+}
+
+const getRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+const start = () => {
+  countSquares = getRandom(min, max);
+  playground.innerHTML = `${countSquares}`;
+  renderBox(countSquares);
+}
+
+
+
+
+/*****************
+События
+*****************/
+h1.addEventListener('click', start)
+
+console.log('end frontend200tb three elements');
